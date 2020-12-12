0a3a9e47ec16d5b70e070b26e6a605f63d42c6f4
Isaac Kelly <isaacmkelly0@gmail.com>
2020-12-12 17:11:08 -0600

    wrote the pseudo code for the primary functions

---
+++ main.js
@@ -0,0 +1,25 @@
+//pseudo code
+const getLocation = async () => {
+  //take location as argument
+  //callback processData() to fetch JSON and get relevant info
+  //start with an await function that will then console log the info
+  //callback displayWeather() to... display the weather
+  //callback setBackground to change the page's background image to fit
+};
+//start here
+const processData = async () => {
+  //use await to look through JSON data for relevant info
+  //return relevant data to be used in displayWeather
+};
+const displayWeather = async () => {
+  //add weather into #info to .display on the html doc
+};
+const setBackground = async () => {
+  //fetch a gif from giphy
+  //set as background image for the webpage
+};
+//OPTIONAL
+const loadTime = () => {
+  //use date for the moment the info was requested
+  //use a second date to for the moment the info was retrieved
+};
