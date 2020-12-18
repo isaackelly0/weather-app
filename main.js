e06418e5bf9d85ef8a642b6312c9256d3df0731b
Isaac Kelly <isaacmkelly0@gmail.com>
2020-12-17 18:21:22 -0600

    added a function for making multi-word city names url appropriate

--- main.js
+++ main.js
@@ -1,34 +1,53 @@
 //pseudo code
-let apiKey = "3452689cc9b0f58ce88b819c85bd8927";
-const getLocation = async () => {
+const apiKey = "3452689cc9b0f58ce88b819c85bd8927";
+const getLocation = async (city) => {
   //take location as argument
+  //new function to check if city name has multiple words
+  const location = cityName(city);
+
   //callback processData() to fetch JSON and get relevant info
-  //start with an await function that will then console log the info
+  const report = await processData(location);
   //callback displayWeather() to... display the weather
+  displayWeather(report);
   //callback setBackground to change the page's background image to fit
+};
+const cityName = (city) => {
+  //seperate city into multiple words
+  let newName = Array.from(city);
+  let location = "";
+  newName.forEach((item, i) => {
+    if (item === " ") {
+      location += "+";
+    } else {
+      location += item;
+    }
+  });
+
+  return location;
+
+  //spit back encapsulated string with + instead of spaces
 };
 //start here
 const processData = async (location) => {
   //use await to look through JSON data for relevant info
   //return relevant data to be used in displayWeather
-  //try {
-  let response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`,
-    {
-      mode: "cors",
-    }
-  );
-  //console.log(response.ok);
-  const data = await response.json();
-  console.log(data);
-  // } catch (error) {
-  //   console.log(error);
-  // }
+  try {
+    let response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`,
+      {
+        mode: "cors",
+      }
+    );
+    const data = await response.json();
+    //in case of further error testing
+    //console.log(data.weather);
+    return data.weather[0].description; //return weather report as string
+  } catch (error) {
+    console.log(error);
+  }
 };
-processData("Toronto,CA");
-//let success = processData("detroit");
-//console.log(success);
-const displayWeather = async () => {
+getLocation("New Orleans");
+const displayWeather = (report) => {
   //add weather into #info to .display on the html doc
 };
 const setBackground = async () => {
