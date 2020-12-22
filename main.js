a5e76550d070960f9348e2ce82d3279bc7275ac0
Isaac Kelly <isaacmkelly0@gmail.com>
2020-12-22 15:15:18 -0600

    reset works, background sets to a gif from giphy

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ const apiKey = "3452689cc9b0f58ce88b819c85bd8927";
 const getLocation = async (city) => {
   //take location as argument
   //callback processData() to fetch JSON and get relevant info
+  const info = document.getElementById("info");
+  if (info.hasChildNodes()) {
+    info.removeChild(info.firstChild);
+  }
   const report = await processData(city);
   //callback displayWeather() to... display the weather
   displayWeather(report);
-  setBackground();
+  setBackground(report);
   //callback setBackground to change the page's background image to fit
 };
 const reset = () => {};
@@ -29,27 +33,26 @@ const processData = async (location) => {
     console.log(error);
   }
 };
-getLocation("New York");
-//getLocation("Chicago"); need a function that deletes children of former search in #info
+getLocation("new york");
 const displayWeather = (report) => {
   //add weather into #info to .display on the html doc
   const info = document.getElementById("info");
-  //info.appendChild("report");
   const weather = document.createElement("p");
   const text = document.createTextNode(report);
   weather.appendChild(text);
   info.appendChild(weather);
-  //info.appendChild(weather);
 };
-const setBackground = async () => {
+const setBackground = async (report) => {
   //fetch a gif from giphy
   try {
     let response = await fetch(
-      `https://api.giphy.com/v1/gifs/search?api_key=i3kTKKLGsAgKnO0B7ERRTRGNllXj45s2&q=clear sky&limit=25&offset=0&rating=g&lang=en`,
+      `https://api.giphy.com/v1/gifs/search?api_key=i3kTKKLGsAgKnO0B7ERRTRGNllXj45s2&q=${report}&limit=25&offset=0&rating=g&lang=en`,
       { mode: "cors" }
     );
     const data = await response.json();
-    console.log(data);
+    let body = document.querySelector("#body");
+    let bg = data.data[0].images.downsized.url;
+    body.style.cssText = `background-image:url(${bg}); background-repeat:no-repeat; background-size:cover`;
   } catch (error) {
     alert(error);
   }
